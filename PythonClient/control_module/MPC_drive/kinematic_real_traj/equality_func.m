f0e16e755de32a5c0f3d5744357f803dd8fbb141
Anna Nylander <anna.s.nylander@gmail.com>
2018-05-24 10:42:29 +0200

    Remove MPC repo

--- PythonClient/control_module/MPC_drive/kinematic_real_traj/equality_func.m
+++
@@ -1,9 +0,0 @@
-function error = equality_func(s_prime, s, u)
-%CEQ Summary of this function goes here
-%   Detailed explanation goes here
-
-    s_prime_correct = step(s,u);
-    error = sum(abs(s_prime_correct - s_prime));
-
-end
-
